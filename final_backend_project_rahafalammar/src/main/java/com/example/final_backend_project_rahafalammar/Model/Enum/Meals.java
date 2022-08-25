5a5f785df4dfb31360f88e96b1612fad4883e374
AlammarRA <61663908+AlammarRA@users.noreply.github.com>
2022-08-25 08:01:30 +0300

    Delete final_backend_project_rahafalammar directory

--- final_backend_project_rahafalammar/src/main/java/com/example/final_backend_project_rahafalammar/Model/Enum/Meals.java
+++
@@ -1,9 +0,0 @@
-package com.example.final_backend_project_rahafalammar.Model.Enum;
-
-public enum Meals {
-    Breakfast,
-    Lunch,
-    Dinner,
-    Desserts,
-    Snacks
-}
--- final_backend_project_rahafalammar/src/main/java/com/example/final_backend_project_rahafalammar/Response/ApiException.java
+++
@@ -1,8 +0,0 @@
-package com.example.final_backend_project_rahafalammar.Response;
-
-public class ApiException extends RuntimeException{
-
-    public ApiException(String message) {
-        super(message);
-    }
-}
